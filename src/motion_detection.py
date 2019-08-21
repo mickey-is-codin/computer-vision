d00f20f2e77bdd1ead73333ffbc81f38128c46dd
Mickey Smith <smithmick14@gmail.com>
2019-08-20 21:49:14 -0500

    Copied over tutorial code...going to review now

---
+++ src/motion_detection.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import time
+import datetime
+import imutils
+import cv2
+
+import numpy as np
+
+def main():
+    print("Beginning motion detection program...")
+
+    cap = cv2.VideoCapture(0)
+
+    first_frame = None
+
+    while True:
+        ret, frame = cap.read()
+
+        frame = imutils.resize(frame, width=500)
+        gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray  = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        if first_frame is None:
+            first_frame = gray
+            continue
+
+        frame_delta = cv2.absdiff(first_frame, gray)
+        thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(
+            thresh.copy(),
+            cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE
+        )
+        cnts = imutils.grab_contours(cnts)
+
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 500:
+                continue
+
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = "Occupied"
+
+        # draw the text and timestamp on the frame
+        cv2.putText(
+            frame,
+            "Room Status: {}".format(text),
+            (10, 20),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 0, 255),
+            2
+        )
+        cv2.putText(
+            frame,
+            datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+            (10, frame.shape[0] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.35,
+            (0, 0, 255),
+            1
+        )
+
+        # show the frame and record if the user presses a key
+        cv2.imshow("Security Feed", frame)
+        cv2.imshow("Thresh", thresh)
+        cv2.imshow("Frame Delta", frame_delta)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
