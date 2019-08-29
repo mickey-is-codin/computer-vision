cb189688001a3b705359f3008dce24d33ce9427b
Mickey Smith <smithmick14@gmail.com>
2019-08-29 14:21:57 -0500

    Beginning work, trying to get image resolution

--- ece-661/hw1/test-programs/draw-shapes/draw_shapes.cpp
+++ ece-661/hw1/test-programs/draw-shapes/draw_shapes.cpp
@@ -17,16 +17,16 @@ int main(int argc, char ** argv) {
 
     cout << "\nConverting image...";
 
-#ifdef DEBUG
-    cout << "=Compiled in debug mode=" << endl;
-#endif
-
     // Get the input image from command line argument
     string img_path = argv[1];
     cout << img_path << endl;
 
+#ifdef DEBUG
+    cout << "=Compiled in debug mode=" << endl;
+#endif
+
     // Declare images
-    Mat input_img;
+    Mat input_img, output_img;
 
     // Set the input image and check for opening errors
     input_img = imread(img_path, CV_LOAD_IMAGE_COLOR);
@@ -34,11 +34,30 @@ int main(int argc, char ** argv) {
         cout << "Failed to open specified image" << endl;
         return EXIT_FAILURE;
     }
+    output_img = input_img.clone();
+
+    // Get image resolution
+    int im_x, im_y;
+    im_x = output_img.cols;
+    im_y = output_img.rows;
+
+#ifdef DEBUG
+    cout << "x size: " << im_x << "\ty size: " << im_y << endl;
+#endif
+
+    // Drawing
+    // line(
+    //     output_img,         // source
+    //     (0,0),
+    //     (),
+    //     color,
+    //     thickness
+    // )
 
     // Create window and show image
     string window_title = "User Image: " + img_path;
     namedWindow(window_title);
-    imshow(window_title, input_img);
+    imshow(window_title, output_img);
 
     waitKey(0);
 
