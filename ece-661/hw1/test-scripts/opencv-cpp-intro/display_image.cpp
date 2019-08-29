d94a9f019188da3aae9b729f77fca42c64d13355
Mickey Smith <smithmick14@gmail.com>
2019-08-29 11:45:09 -0500

    Starting out syntax and namespaces

--- ece-661/hw1/test-scripts/opencv-cpp-intro/display_image.cpp
+++ ece-661/hw1/test-scripts/opencv-cpp-intro/display_image.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 
+using namespace std;
+using namespace cv;
+
 int main(int arc, char ** argv) {
-    std::cout << "Beginning program..." << std::endl;
+    cout << "Beginning program..." << endl;
+
+
 }
