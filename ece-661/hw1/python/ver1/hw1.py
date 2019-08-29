601d073ef37989ef4a1677082bace7f260ac7b5a
Mickey Smith <smithmick14@gmail.com>
2019-08-29 16:14:56 -0500

    Little syntax stuff, last commit at the office

--- ece-661/hw1/python/ver1/hw1.py
+++ ece-661/hw1/python/ver1/hw1.py
@@ -1,8 +1,18 @@
-import numpy as np
 import cv2
+import argparse
+
+import numpy as np
 
 def main():
-    print('\nBeginning program...\n')
+
+    parser = argparse.ArgumentParser(description='Program to remove projective transformations from an image')
+    parser.add_argument('-o', '--output', action='store', dest='output_path', required=True, help='input file')
+    parser.add_argument('-i', '--input',  action='store', dest='input_path',  required=True, help='output file')
+    args = parser.parse_args()
+
+    print('\nRemoving distortion from {}...\n'.format(args.input_path))
+
+
 
 if __name__ == '__main__':
     main()
