a7ea923398740ea2c77b652415396c69eef0abae
Mickey Smith <smithmick14@gmail.com>
2019-08-29 12:07:24 -0500

    Added error handling and mat object

--- ece-661/hw1/test-scripts/opencv-cpp-intro/display_image.cpp
+++ ece-661/hw1/test-scripts/opencv-cpp-intro/display_image.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
-#include <opencv2/core.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <cstdlib>
 
 using namespace std;
 using namespace cv;
 
-int main(int arc, char ** argv) {
+int main(int argc, char ** argv) {
 
-    cout << "Beginning program..." << endl;
+    if (argc != 2) {
+        cout << "Usage: image-display image" << endl;
+        return EXIT_FAILURE;
+    }
+
+    cout << "\nDisplaying image...";
 
 #ifdef DEBUG
     cout << "=Compiled in debug mode=" << endl;
 #endif
 
+    string img_path = argv[1];
+    cout << img_path << endl;
+
+    Mat input_img;
+    input_img = imread(img_path, CV_LOAD_IMAGE_COLOR);
+
+    if (!input_img.data) {
+        cout << "Failed to open specified image" << endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
