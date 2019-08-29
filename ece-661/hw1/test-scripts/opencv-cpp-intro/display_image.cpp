077270e7251df5ce2dcdfbc53df913981d0d7880
Mickey Smith <smithmick14@gmail.com>
2019-08-29 11:52:29 -0500

    Added debug mode and setup basic makefile

--- ece-661/hw1/test-scripts/opencv-cpp-intro/display_image.cpp
+++ ece-661/hw1/test-scripts/opencv-cpp-intro/display_image.cpp
@@ -5,7 +5,11 @@ using namespace std;
 using namespace cv;
 
 int main(int arc, char ** argv) {
+
     cout << "Beginning program..." << endl;
 
+#ifdef DEBUG
+    cout << "=Compiled in debug mode=" << endl;
+#endif
 
 }
