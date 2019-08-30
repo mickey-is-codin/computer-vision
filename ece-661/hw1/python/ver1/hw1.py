c9fccd2fe27832e4a73a926b5f065033383ea151
Mickey Smith <smithmick14@gmail.com>
2019-08-30 10:01:30 -0500

    Projective transformation removed for specific region, need to work on manualk version

--- ece-661/hw1/python/ver1/hw1.py
+++ ece-661/hw1/python/ver1/hw1.py
@@ -17,20 +17,70 @@ def main():
     print('Input image is a {}-channel {}x{} image'
         .format(image_info['channels'], image_info['width'], image_info['height']))
 
-    im_rect = {
-        'im_ul_crnr' : (182,55),
-        'im_ur_crnr' : (316,96),
-        'im_bl_crnr' : (154,440),
-        'im_br_crnr' : (296,442),
+    im_rect_pts = {
+        'ul' : (182, 55),
+        'ur' : (316, 96),
+        'bl' : (154, 440),
+        'br' : (296, 442)
     }
 
-    plot_im_corners(input_img, im_rect)
-    plot_im_edges(input_img, im_rect)
+    im_corner_vectors = {
+        'ul' : np.transpose(np.array([182, 55,  1])),
+        'ur' : np.transpose(np.array([316, 96,  1])),
+        'bl' : np.transpose(np.array([154, 440, 1])),
+        'br' : np.transpose(np.array([296, 442, 1]))
+    }
 
-    # Get the dot product to find the angle b/wn
-    # Math this out in notebook first
+    print('\nCorner Points as Vectors: ')
+    for k, v in im_corner_vectors.items():
+        print('{}: {}, shape: {}'.format(k,v, v.shape))
+
+    '''
+             D
+         -------->
+        ^         ^
+       A|         |C
+        |         |
+         -------->
+             B
+    '''
+
+    vec_a = np.cross(im_corner_vectors['bl'],im_corner_vectors['ul'])
+    vec_b = np.cross(im_corner_vectors['bl'],im_corner_vectors['br'])
+    vec_c = np.cross(im_corner_vectors['br'],im_corner_vectors['ur'])
+    vec_d = np.cross(im_corner_vectors['ul'],im_corner_vectors['ur'])
+
+    ver_van_pt = np.cross(vec_a, vec_c)
+    hor_van_pt = np.cross(vec_b, vec_d)
+
+    van_line = np.cross(ver_van_pt, hor_van_pt)
+    print('Vanishing line: {}, shape: {}'.format(van_line, van_line.shape))
+
+    # Manual
+    # homography = np.identity(len(van_line))
+    # homography[-1,:] = van_line
+    # print('Homography: \n{}'.format(homography))
+
+    # OpenCV Auto
+    image_points = np.float32([value[0:2] for key, value in im_corner_vectors.items()])
+    world_points = np.float32([[0,0], [300,0], [0,300], [300,300]])
+    homography = cv2.getPerspectiveTransform(image_points, world_points)
+
+    output_shape = (input_img.shape[0], input_img.shape[1])
+    output_img = cv2.warpPerspective(
+        input_img,
+        M=homography,
+        dsize=(300,300)
+    )
+
+    plot_im_corners(input_img, im_rect_pts)
+    plot_im_edges(input_img, im_rect_pts)
 
     cv2.imshow('Input Image', input_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cv2.imshow('Output Image', output_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -38,16 +88,16 @@ def plot_im_edges(input_img, rect_points):
 
     line_1 = cv2.line(
         input_img,
-        pt1=rect_points['im_bl_crnr'],
-        pt2=rect_points['im_ul_crnr'],
+        pt1=rect_points['bl'],
+        pt2=rect_points['ul'],
         color=(0,0,255),
         thickness=2
     )
 
     line_2 = cv2.line(
         input_img,
-        pt1=rect_points['im_bl_crnr'],
-        pt2=rect_points['im_br_crnr'],
+        pt1=rect_points['bl'],
+        pt2=rect_points['br'],
         color=(0,0,255),
         thickness=2
     )
@@ -56,7 +106,7 @@ def plot_im_corners(input_img, rect_points):
 
     cv2.circle(
         input_img,
-        center=rect_points['im_ul_crnr'],
+        center=rect_points['ul'],
         radius=3,
         color=(255,0,0),
         thickness=3
@@ -64,7 +114,7 @@ def plot_im_corners(input_img, rect_points):
 
     cv2.circle(
         input_img,
-        center=rect_points['im_ur_crnr'],
+        center=rect_points['ur'],
         radius=3,
         color=(255,0,0),
         thickness=3
@@ -72,7 +122,7 @@ def plot_im_corners(input_img, rect_points):
 
     cv2.circle(
         input_img,
-        center=rect_points['im_bl_crnr'],
+        center=rect_points['bl'],
         radius=3,
         color=(255,0,0),
         thickness=3
@@ -80,7 +130,7 @@ def plot_im_corners(input_img, rect_points):
 
     cv2.circle(
         input_img,
-        center=rect_points['im_br_crnr'],
+        center=rect_points['br'],
         radius=3,
         color=(255,0,0),
         thickness=3
