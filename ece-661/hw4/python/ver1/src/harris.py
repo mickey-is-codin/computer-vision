171643fda472fa5c080f3b0fc8fe2d32b88e9651
Mickey Smith <smithmick14@gmail.com>
2019-09-09 16:10:32 -0500

    C matrix method isn't working well. Going to change to using opencv's documentation with M matrix

--- ece-661/hw4/python/ver1/src/harris.py
+++ ece-661/hw4/python/ver1/src/harris.py
@@ -20,35 +20,44 @@ def main():
 
     dx, dy = compute_derivatives(gray_input_img)
 
-    detect_corners(gray_input_img, dx, dy)
+    detect_corners(gray_input_img, output_img, dx, dy)
 
-    show_results(gray_input_img, dx)
+    show_results(gray_input_img, output_img)
 
-def detect_corners(img, dx, dy):
+def detect_corners(input_img, output_img, dx, dy):
+
+    corner_thresh = 0.3
+
+    #ratios = np.array([])
 
     pixel_count = 0
-    for im_y in range(img.shape[0]):
-        for im_x in range(img.shape[1]):
+    for im_y in range(input_img.shape[0]):
+        for im_x in range(input_img.shape[1]):
 
-            #print("Getting C matrix for point ({},{}): {}".format(im_x, im_y, img[im_y, im_x]))
             c = np.zeros((2,2))
 
-            for nb_y in range(-2,3):
-                for nb_x in range(-2,3):
-                    #print("Neighbor ({},{})".format(nb_x, nb_y))
-                    #print("Indexing from derivative images at ({},{})".format(im_y + nb_y, im_x + nb_x))
-                    if (im_y + nb_y > 0)            and \
-                       (im_x + nb_x > 0)            and \
-                       (im_y + nb_y < img.shape[0]) and \
-                       (im_x + nb_x < img.shape[1]):
-                        #print()
-                        c[0,0] += np.square(dx[im_y + nb_y, im_x + nb_x])
-                        c[0,1] += np.multiply(dx[im_y + nb_y, im_x + nb_x], dy[im_y + nb_y, im_x + nb_x])
-                        c[1,0] += np.multiply(dx[im_y + nb_y, im_x + nb_x], dy[im_y + nb_y, im_x + nb_x])
-                        c[1,1] += np.square(dx[im_y + nb_y, im_x + nb_x])
+            c[0,0] = np.sum(np.square(dx[im_y-2:im_y+2, im_x-2:im_y+2]))
+            c[0,1] = np.sum(np.multiply(dx[im_y-2:im_y+2, im_x-2:im_y+2], dy[im_y-2:im_y+2, im_x-2:im_y+2]))
+            c[1,0] = c[0,1]
+            c[1,1] = np.sum(np.square(dy[im_y-2:im_y+2, im_x-2:im_y+2]))
 
-            #print(c)
-            #return
+            det_c   = (c[0,0] * c[1,1]) - (c[0,1] * c[1,0])
+            trace_c = c[0,0] + c[1,1]
+
+            if np.sum(c) > 0:
+                ratio = np.abs(det_c / (trace_c ** 2))
+                #ratios = np.append(ratios, ratio)
+
+                if (ratio > corner_thresh):
+                    output_img[im_y, im_x] = [0, 0, 255]
+
+            pixel_count += 1
+
+    # print(np.mean(ratios))
+    # print(np.max(ratios))
+    # print(np.min(ratios))
+    # print(np.percentile(ratios, 99))
+    # print(np.std(ratios))
 
 
 def compute_derivatives(img):
